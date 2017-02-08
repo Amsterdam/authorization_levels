a4b35f1a54e9c6cdb308a19fbf0bb329d3bdbe4f
Evert Lammerts <evert@lucipher.net>
2017-02-08 15:44:21 +0100

    bump to dev2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ py_modules = ['authorization_levels']
 
 setup(
     name='datapunt-authorization-levels',
-    version='0.1.0.dev1',
+    version='0.1.0.dev2',
     description='Datapunt authorization levels',
     url='https://github.com/DatapuntAmsterdam/autorization_levels',
     author='Amsterdam Datapunt',
